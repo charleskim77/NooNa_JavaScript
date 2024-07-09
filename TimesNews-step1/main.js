66b8fd2b5c20e072cdc12ba086538fae916eedb4
CharlesKim <tot822@naver.com>
2024-07-09 17:25:54 +0900

    20240709

--- TimesNews-step1/main.js
+++ TimesNews-step1/main.js
@@ -1,29 +1,14 @@
 let news = [];
-
-const getApiKey = () => 
-    fetch('get_api_key.php')
-        .then(response => response.json())
-        .then(data => data.api_key);
+const API_KEY =`bad7a21cecaa4a4babf650767c4a2b98`;
 
 const getLatestNews = async () => {
-    try {
-        const apiKey = await getApiKey();
-        console.log("apikey", apiKey);
-
-        const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`);
+        const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
         console.log("url", url);
 
         const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
         const data = await response.json();
         news = data.articles;
         console.log("News data:", news);
-    } catch (error) {
-        console.error("Error fetching news:", error);
-    }
 };
 
 getLatestNews();
